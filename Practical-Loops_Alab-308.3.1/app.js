0f0003528becf074e9f586c5420d7e129199fdfc
Julio <youremail@example.com>
2025-06-18 17:04:09 -0400

    308.3.1 Practical Loops

---
+++ Practical-Loops_Alab-308.3.1/app.js
@@ -0,0 +1,4 @@
+// Use for loops to iterate a specific number of times.
+// Use for of loops to iterate through iterable data-like strings.
+// Use while loops to iterate based on a condition.
+// Use the break and continue statements to control loop flow.
