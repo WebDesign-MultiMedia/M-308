0a10f6b15ebadbd3c956377cdad0bd48ec89369e
Julio <youremail@example.com>
2025-06-30 15:28:44 -0400

    Part 3 tried

--- Alab-308.5.1/app.js
+++ Alab-308.5.1/app.js
@@ -54,3 +54,59 @@ function fac(n) {
 }
 
 fac(10);
+
+
+// 
+// PART TWO
+
+const obj = [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+ { id: "48", name: "Barry", occupation: "Runner", age: "25" },
+ { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+ { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+ { id: "7", name: "Bilbo", occupation: "None", age: "111" }];
+
+//  Sort the array by age.
+let sorted = obj.sort((a, b) => a.age - b.age);
+console.log("Sorted by age: ", sorted);
+
+// Filter the array to remove entries with an age greater than 50.
+const filertedPeople = obj.filter(p => p.age >= 50);
+console.log("age greater than 50", filertedPeople);
+
+// Map the array to change the “occupation” key to “job” and increment every age by 1.
+const transformedArray = obj.map(p => {
+  const newObj = { ...p };
+  newObj.job = newObj.occupation;
+  delete newObj.occupation;
+  newObj.age = parseInt(newObj.age) + 1;
+  return newObj;
+});
+
+console.log("",transformedArray);
+
+// Use the reduce method to calculate the sum of the ages.
+
+ const sumOfAge = obj.reduce((acc, p) =>{
+    return acc + parseInt(p.age);
+ }, 0);
+
+ console.log("Sum of ages : " , sumOfAge);
+ 
+//  
+// PART THREE
+
+// Take an object and increment its age field.
+const inc = obj.reduce((acc, a) =>{
+    let x = parseInt(a.age) + 1;
+console.log("incremented ages: ", x);
+})
+
+// Take an object, make a copy, and increment the age field of the copy. Return the copy.
+console.log(" ");
+
+// Copy
+const copiedObj = { ...obj };
+console.log("Copied obj: ", copiedObj);
+
+
+
